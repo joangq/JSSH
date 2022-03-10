c18a5d54e9c2c3606e2832bca6989499a0808c8e
Raykza <86327859+Joan-GQ@users.noreply.github.com>
2022-03-10 07:50:52 -0300

    Added connect() to Session

--- src/main/java/me/joan/jssh/Session.java
+++ src/main/java/me/joan/jssh/Session.java
@@ -2,6 +2,8 @@ package me.joan.jssh;
 
 import com.jcraft.jsch.*;
 
+import java.util.Properties;
+
 public class Session {
 
     // All the variables for the JSch session to connect
@@ -24,6 +26,38 @@ public class Session {
         this.pubkey = pubkey;
         this.passphrase = passphrase;
         this.logger = logger;
+    }
+
+    // Main method to connect
+    /* TODO: This is only a method for connecting using a keypair. There should be multiple methods for
+     *   connecting using different security configurations! */
+
+    /* Note: JSch can only use old PEM formatted keys. If you don't use such formatted key, it's possible that JSch
+     * throws an "Invalid key" error.
+     * To format old-existing keys it's recommended to use PuTTYgen's "Import/Export key" options.
+     */
+    public void connect() {
+        JSch jSch = new JSch();
+
+        try {
+            jSch.addIdentity(this.privkey, this.pubkey, this.passphrase);
+
+            this.session = jSch.getSession(this.username, this.hostname, this.port);
+            this.session.setConfig("PreferredAuthentication", "publickey,keyboard-interactive,password");
+
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking","no");
+
+            this.session.setConfig(config);
+            this.session.setPassword(this.password);
+
+            System.out.println("Connecting SSH to " + this.username + "@" + this.hostname + " - Please wait for few seconds...");
+            this.session.connect();
+            System.out.println("Connected successfully!");
+        } catch (JSchException jSchException) {
+            System.out.println("There has been an error while connecting to " + this.username +"@"+this.hostname);
+            jSchException.printStackTrace();
+        }
     }
 
 }
