9c63008f01b08bdb2486b6fc950e11aa1b184be7
Raykza <86327859+Joan-GQ@users.noreply.github.com>
2022-03-10 08:59:26 -0300

    Added example code

--- src/main/java/me/joan/Main.java
+++ src/main/java/me/joan/Main.java
@@ -1,4 +1,23 @@
 package me.joan;
 
+import java.io.File;
+import me.joan.jssh.Session;
+import me.joan.util.PropertiesManager;
+import me.joan.util.Utils;
+import org.apache.logging.log4j.*;
+
 public class Main {
+    public static Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("JSSH Client starting...");
+        PropertiesManager propertiesManager = new PropertiesManager(LOGGER);
+        propertiesManager.load(new File("./session.properties"));
+
+        Session JSSH = new Session(propertiesManager.getKeys(), LOGGER);
+        JSSH.connect();
+        JSSH.logExecuteCommand("echo Hello from the other side!");
+    }
+
+    public Main() {}
 }
